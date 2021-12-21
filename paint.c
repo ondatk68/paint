1e9dd47b01ced0e3c2ea5619cfbb58a8f2773853
恩田健太郎 <ondakentaro0805@g.ecc.u-tokyo.ac.jp>
2021-12-22 00:53:39 +0900

    基本課題2まで

--- paint.c
+++ paint.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <errno.h> // for error catch
+#include <math.h>
 
 // Structure for canvas
 typedef struct
@@ -22,8 +23,7 @@ struct command{
     Command *next;
 };
 
-typedef struct
-{
+typedef struct{
     Command *begin;
     size_t bufsize; // [*] : この方が効率的ですね。一部の方から指摘ありました。
 } History;
@@ -40,7 +40,7 @@ void clear_command(void);
 void clear_screen(void);
 
 // enum for interpret_command results
-typedef enum res{ EXIT, LINE, UNDO, SAVE, UNKNOWN, ERRNONINT, ERRLACKARGS, NOCOMMAND} Result;
+typedef enum res{ EXIT, LINE, RECT, CIRCLE, UNDO, LOAD, SAVE, UNKNOWN, ERRNONINT, ERRLACKARGS, NOCOMMAND, NOFILE} Result;
 // Result 型に応じて出力するメッセージを返す
 char *strresult(Result res);
 
@@ -64,22 +64,22 @@ int main(int argc, char **argv)
     int width;
     int height;
     if (argc != 3){
-	fprintf(stderr,"usage: %s <width> <height>\n",argv[0]);
-	return EXIT_FAILURE;
+        fprintf(stderr,"usage: %s <width> <height>\n",argv[0]);
+        return EXIT_FAILURE;
     } else{
-	char *e;
-	long w = strtol(argv[1],&e,10);
-	if (*e != '\0'){
-	    fprintf(stderr, "%s: irregular character found %s\n", argv[1],e);
-	    return EXIT_FAILURE;
-	}
-	long h = strtol(argv[2],&e,10);
-	if (*e != '\0'){
-	    fprintf(stderr, "%s: irregular character found %s\n", argv[2],e);
-	    return EXIT_FAILURE;
-	}
-	width = (int)w;
-	height = (int)h;    
+        char *e;
+        long w = strtol(argv[1],&e,10);
+        if (*e != '\0'){
+            fprintf(stderr, "%s: irregular character found %s\n", argv[1],e);
+            return EXIT_FAILURE;
+        }
+        long h = strtol(argv[2],&e,10);
+        if (*e != '\0'){
+            fprintf(stderr, "%s: irregular character found %s\n", argv[2],e);
+            return EXIT_FAILURE;
+        }
+        width = (int)w;
+        height = (int)h;    
     }
     char pen = '*';
     
@@ -91,29 +91,29 @@ int main(int argc, char **argv)
     printf("\n"); // required especially for windows env
 
     while(1){
-	// [*]
-	// hsize はひとまずなし
-	// 作る場合はリスト長を調べる関数を作っておく
-	print_canvas(c);
-	printf("* > ");
-	if(fgets(buf, bufsize, stdin) == NULL) break;
-	
-	const Result r = interpret_command(buf, &his, c);
+        // [*]
+        // hsize はひとまずなし
+        // 作る場合はリスト長を調べる関数を作っておく
+        print_canvas(c);
+        printf("* > ");
+        if(fgets(buf, bufsize, stdin) == NULL) break;
+        
+        const Result r = interpret_command(buf, &his, c);
 
-	if (r == EXIT) break;
+        if (r == EXIT) break;
 
-	// 返ってきた結果に応じてコマンド結果を表示
-	clear_command();
-	printf("%s\n",strresult(r));
-	// LINEの場合はHistory構造体に入れる
-	if (r == LINE) {
-	    // [*]
-	    push_command(&his,buf);
-	}
-	
-	rewind_screen(2); // command results
-	clear_command(); // command itself
-	rewind_screen(height+2); // rewind the screen to command input
+        // 返ってきた結果に応じてコマンド結果を表示
+        clear_command();
+        printf("%s\n",strresult(r));
+        // LINEの場合はHistory構造体に入れる
+        if (r == LINE || r == RECT || r == CIRCLE) {
+            // [*]
+            push_command(&his,buf);
+        }
+        
+        rewind_screen(2); // command results
+        clear_command(); // command itself
+        rewind_screen(height+2); // rewind the screen to command input
 	
     }
     
@@ -133,7 +133,7 @@ Canvas *init_canvas(int width,int height, char pen)
     char *tmp = (char *)malloc(width*height*sizeof(char));
     memset(tmp, ' ', width*height*sizeof(char));
     for (int i = 0 ; i < width ; i++){
-	new->canvas[i] = tmp + i * height;
+	    new->canvas[i] = tmp + i * height;
     }
     
     new->pen = pen;
@@ -156,74 +156,85 @@ void print_canvas(Canvas *c)
     
     // 上の壁
     printf("+");
-    for (int x = 0 ; x < width ; x++)
-	printf("-");
+    for (int x = 0 ; x < width ; x++){
+	    printf("-");
+    }
     printf("+\n");
     
     // 外壁と内側
     for (int y = 0 ; y < height ; y++) {
-	printf("|");
-	for (int x = 0 ; x < width; x++){
-	    const char c = canvas[x][y];
-	    putchar(c);
-	}
-	printf("|\n");
+        printf("|");
+        for (int x = 0 ; x < width; x++){
+            const char c = canvas[x][y];
+            putchar(c);
+        }
+        printf("|\n");
     }
     
     // 下の壁
     printf( "+");
-    for (int x = 0 ; x < width ; x++)
-	printf("-");
+    for (int x = 0 ; x < width ; x++){
+	    printf("-");
+    }
     printf("+\n");
     fflush(stdout);
 }
 
-void free_canvas(Canvas *c)
-{
+void free_canvas(Canvas *c){
     free(c->canvas[0]); //  for 2-D array free
     free(c->canvas);
     free(c);
 }
 
-void rewind_screen(unsigned int line)
-{
+void rewind_screen(unsigned int line){
     printf("\e[%dA",line);
 }
 
-void clear_command(void)
-{
+void clear_command(void){
     printf("\e[2K");
 }
 
-void clear_screen(void)
-{
+void clear_screen(void){
     printf( "\e[2J");
 }
 
 
-int max(const int a, const int b)
-{
+int max(const int a, const int b){
     return (a > b) ? a : b;
 }
-void draw_line(Canvas *c, const int x0, const int y0, const int x1, const int y1)
-{
+
+void draw_line(Canvas *c, const int x0, const int y0, const int x1, const int y1){
     const int width = c->width;
     const int height = c->height;
     char pen = c->pen;
     
     const int n = max(abs(x1 - x0), abs(y1 - y0));
-    if ( (x0 >= 0) && (x0 < width) && (y0 >= 0) && (y0 < height))
-	c->canvas[x0][y0] = pen;
+    if ( (x0 >= 0) && (x0 < width) && (y0 >= 0) && (y0 < height)){
+        c->canvas[x0][y0] = pen;
+    }
     for (int i = 1; i <= n; i++) {
-	const int x = x0 + i * (x1 - x0) / n;
-	const int y = y0 + i * (y1 - y0) / n;
-	if ( (x >= 0) && (x< width) && (y >= 0) && (y < height))
-	    c->canvas[x][y] = pen;
+        const int x = x0 + i * (x1 - x0) / n;
+        const int y = y0 + i * (y1 - y0) / n;
+        if ( (x >= 0) && (x< width) && (y >= 0) && (y < height)){
+            c->canvas[x][y] = pen;
+        }
     }
 }
 
-void save_history(const char *filename, History *his)
-{
+void draw_circle(Canvas *c, const int x0, const int y0, const int r){
+    const int width = c->width;
+    const int height = c->height;
+    char pen=c->pen;
+    for(int x=0; x<width; x++){
+        for(int y=0; y<height; y++){
+            if(pow(r-0.5,2) < pow(x-x0,2)+pow(y-y0,2) && pow(x-x0,2)+pow(y-y0,2)<pow(r+0.5,2)){
+                c->canvas[x][y]=pen;
+            }
+        }
+    }
+}
+
+void save_history(const char *filename, History *his){
     const char *default_history_file = "history.txt";
     if (filename == NULL)
 	filename = default_history_file;
@@ -245,72 +256,152 @@ Result interpret_command(const char *command, History *his, Canvas *c)
 {
     char buf[his->bufsize];
     strcpy(buf, command);
-    buf[strlen(buf) - 1] = 0; // remove the newline character at the end
+    if(buf[strlen(buf) - 1]=='\n'){
+        buf[strlen(buf) - 1]=0;
+    } // remove the newline character at the end
     
     const char *s = strtok(buf, " ");
     if (s == NULL){ // 改行だけ入力された場合
-	return UNKNOWN;
+	    return UNKNOWN;
     }
     // The first token corresponds to command
     if (strcmp(s, "line") == 0) {
-	int p[4] = {0}; // p[0]: x0, p[1]: y0, p[2]: x1, p[3]: x1 
-	char *b[4];
-	for (int i = 0 ; i < 4; i++){
-	    b[i] = strtok(NULL, " ");
-	    if (b[i] == NULL){
-		return ERRLACKARGS;
-	    }
-	}
-	for (int i = 0 ; i < 4 ; i++){
-	    char *e;
-	    long v = strtol(b[i],&e, 10);
-	    if (*e != '\0'){
-		return ERRNONINT;
-	    }
-	    p[i] = (int)v;
-	}
-	
-	draw_line(c,p[0],p[1],p[2],p[3]);
-	return LINE;
+        int p[4] = {0}; // p[0]: x0, p[1]: y0, p[2]: x1, p[3]: y1 
+        char *b[4];
+        for (int i = 0 ; i < 4; i++){
+            b[i] = strtok(NULL, " ");
+            if (b[i] == NULL){
+                return ERRLACKARGS;
+            }
+        }
+        for (int i = 0 ; i < 4 ; i++){
+            char *e;
+            long v = strtol(b[i],&e, 10);
+            if (*e != '\0'){
+                return ERRNONINT;
+            }
+            p[i] = (int)v;
+        }
+        
+        draw_line(c,p[0],p[1],p[2],p[3]);
+        return LINE;
     }
-    
+    if(strcmp(s, "rect")==0){
+        int p[4] = {0}; // p[0]: x0, p[1]: y0, p[2]: width, p[3]: height 
+        char *b[4];
+        for (int i = 0 ; i < 4; i++){
+            b[i] = strtok(NULL, " ");
+            if (b[i] == NULL){
+                return ERRLACKARGS;
+            }
+        }
+        for (int i = 0 ; i < 4 ; i++){
+            char *e;
+            long v = strtol(b[i],&e, 10);
+            if (*e != '\0'){
+                return ERRNONINT;
+            }
+            p[i] = (int)v;
+        }
+        draw_line(c,p[0],p[1],p[0]+p[2], p[1]);
+        draw_line(c,p[0]+p[2], p[1],p[0]+p[2], p[1]+p[3]);
+        draw_line(c,p[0]+p[2], p[1]+p[3], p[0], p[1]+p[3]);
+        draw_line(c,p[0], p[1]+p[3], p[0],p[1]);
+        return RECT;
+    }
+
+    if(strcmp(s,"circle")==0){
+        int p[3] = {0}; // p[0]: x0, p[1]: y0, p[2]: r 
+        char *b[3];
+        for (int i = 0 ; i < 3; i++){
+            b[i] = strtok(NULL, " ");
+            if (b[i] == NULL){
+                return ERRLACKARGS;
+            }
+        }
+        for (int i = 0 ; i < 3 ; i++){
+            char *e;
+            long v = strtol(b[i],&e, 10);
+            if (*e != '\0'){
+                return ERRNONINT;
+            }
+            p[i] = (int)v;
+        }
+        draw_circle(c,p[0],p[1],p[2]);
+        return CIRCLE;
+    }
+
+    if(strcmp(s,"load")==0){
+        FILE *fp;
+        char *tmp;
+        char *filename;
+        if((tmp=strtok(NULL, " "))==NULL){
+            strcpy(filename, "history.txt");
+        }else{
+            strcpy(filename, tmp);
+        }
+        
+        if((fp=fopen(filename, "r"))==NULL){
+	        return NOFILE;
+        }else{
+
+            while(1){
+                
+                if(fgets(buf, his->bufsize, fp) == NULL) break;
+                
+                const Result r = interpret_command(buf, his, c);
+
+                if (r == EXIT) break;
+
+                // LINEの場合はHistory構造体に入れる
+                if (r == LINE || r == RECT || r == CIRCLE) {
+                    push_command(his,buf);
+                }
+            }
+
+        }
+        fclose(fp);
+        return LOAD;
+    }
+
     if (strcmp(s, "save") == 0) {
-	s = strtok(NULL, " ");
-	save_history(s, his);
-	return SAVE;
+        s = strtok(NULL, " ");
+        save_history(s, his);
+        return SAVE;
     }
     
     if (strcmp(s, "undo") == 0) {
-	reset_canvas(c);
-	//[*] 線形リストの先頭からスキャンして逐次実行
-	// pop_back のスキャン中にinterpret_command を絡めた感じ
-	Command *p = his->begin;
-	if (p == NULL){
-	    return NOCOMMAND;
-	}
-	else{
-	    Command *q = NULL; // 新たな終端を決める時に使う
-	    while (p->next != NULL){ // 終端でないコマンドは実行して良い
-		interpret_command(p->str, his, c);
-		q = p;
-		p = p->next;
-	    }
-	    // 1つしかないコマンドのundoではリストの先頭を変更する
-	    if (q == NULL) {
-		his->begin = NULL;
-	    }
-	    else{
-		q->next = NULL;
-	    }
-	    free(p->str);
-	    free(p);	
-	    return UNDO;
-	}  
+        reset_canvas(c);
+        //[*] 線形リストの先頭からスキャンして逐次実行
+        // pop_back のスキャン中にinterpret_command を絡めた感じ
+        Command *p = his->begin;
+        if (p == NULL){
+            return NOCOMMAND;
+        }
+        else{
+            Command *q = NULL; // 新たな終端を決める時に使う
+            while (p->next != NULL){ // 終端でないコマンドは実行して良い
+            interpret_command(p->str, his, c);
+            q = p;
+            p = p->next;
+            }
+            // 1つしかないコマンドのundoではリストの先頭を変更する
+            if (q == NULL) {
+            his->begin = NULL;
+            }
+            else{
+            q->next = NULL;
+            }
+            free(p->str);
+            free(p);	
+            return UNDO;
+        }  
     }
     
     if (strcmp(s, "quit") == 0) {
-	return EXIT;
+	    return EXIT;
     }
+
     return UNKNOWN;
 }
 
@@ -345,6 +436,12 @@ char *strresult(Result res){
 	return "history saved";
     case LINE:
 	return "1 line drawn";
+    case RECT:
+    return "1 rectangle drawn";
+    case CIRCLE:
+    return "1 circle drawn";
+    case LOAD:
+    return "successfully loaded";
     case UNDO:
 	return "undo!";
     case UNKNOWN:
@@ -355,6 +452,8 @@ char *strresult(Result res){
 	return "Too few arguments";
     case NOCOMMAND:
 	return "No command in history";
+    case NOFILE:
+    return "no such file";
     }
     return NULL;
 }
